a95055eebfc6848e2fcdf11b2ce4026502304f45
olasrum <mursalo@gmail.com>
2022-10-08 15:39:05 +1000

    Done with assignment

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({sushis, onEatSushi}) {
+  const [sushiIndex, setSushiIndex] = useState(0);
+
+  const sushiItems = sushis
+    .slice(sushiIndex, sushiIndex + 4)
+    .map((sushi) => (
+      <Sushi key={sushi.id} sushi={sushi} onEatSushi={onEatSushi} />
+    ));
+
+    function handleClickMore() {
+      setSushiIndex((sushiIndex) => (sushiIndex +4));
+    }
+  
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiItems}
+      <MoreButton onClickMore={handleClickMore}/>
     </div>
   );
 }
